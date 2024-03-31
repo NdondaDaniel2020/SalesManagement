aa321aa2efd6fea3bbec5b899214931db43c4392
Nd Daniel <108186371+NdondaDaniel2020@users.noreply.github.com>
2024-04-01 00:01:52 +0100

    melhoria do gerador de código de chave, para não fornecer código repetidos. metodo que salva as configurações dos tamanho das imagens. E metodo para adicionar produtos na base de dados

--- src/gui/function/functions_main_window/static_functions.py
+++ src/gui/function/functions_main_window/static_functions.py
@@ -1,13 +1,26 @@
 import os
+import json
 import random
+from src.gui.core.absolute_path import AbsolutePath
+from src.gui.core.database import DataBase
 
 
 def generate_barcode() -> str:
     codigo: str = ""
-    for _ in range(13):
-        codigo += str(random.randint(0, 9))
 
-    return codigo
+    db = DataBase(AbsolutePath().getPathDatabase())
+    db.connectDataBase()
+    query = db.executarFetchall("SELECT chave FROM produto")
+    db.disconnectDataBase()
+
+    while True:
+        for _ in range(13):
+            codigo += str(random.randint(0, 9))
+
+        if not codigo in query:
+            return codigo
+        else:
+            codigo = ''
 
 
 def reduce_url(url: str) -> str:
@@ -19,3 +32,48 @@ def reduce_url(url: str) -> str:
         return url
 
     return url
+
+
+def saveImageSettingInSizeSetting(produto: dict):
+    json_file = AbsolutePath().getPathSettingSize()
+    dados: dict = {}
+
+    with open(json_file, 'r') as file:
+        dados = json.load(file)
+
+    if not produto['nome_produto'] in dados:
+        nome = produto['nome_produto'].lower()
+        dados[nome] = produto['size_image']
+
+        with open(json_file, "w") as file:
+            json.dump(dados, file)
+
+
+def insertProductDataIntoTheDatabase(produto: dict):
+    insert_produto = fr"""INSERT INTO produto (chave, nome, preco_venda, quantidade, unidade, categoria,linkImg,
+                                        informacoes_adicionais)
+                                VALUES ('{produto['chave']}', '{produto['nome_produto']}', {produto['preco_venda']},
+                                        {produto['quantidade']}, {produto['unidade']}, {produto['categoria']},
+                                        '{produto['linkImg']}' , '{produto['informacoes_adicionais']}')
+               """
+
+    db = DataBase(AbsolutePath().getPathDatabase())
+    db.connectDataBase()
+    db.executarComand(insert_produto)
+
+    if produto['data_de_expiracao']:
+        id_produto = db.executarFetchone("SELECT id FROM produto ORDER By id DESC LIMIT 1")
+        for dado in produto['data_de_expiracao']:
+            insert_data_expiracao = fr"""INSERT INTO data_de_expiracao(data, id_produto, informacoes_de_expiracao)
+                                                              VALUES ('{dado[0]}', {id_produto[0]}, '{dado[1]}')"""
+            db.executarComand(insert_data_expiracao)
+
+    db.disconnectDataBase()
+
+
+
+if __name__ == '__main__':
+    # produto = {'linkImg': 'C:\\Users\\Daniel\\Downloads\\barra_de_cera.png', 'size_image': {'image': (260, 260), 'icon_image': (30, 30)}, 'data_de_expiracao': [], 'unidade': 4, 'categoria': 2, 'nome_produto': 'barra de cera', 'preco_venda': 12000, 'chave': '8978885538803', 'quantidade': 12, 'informacoes_adicionais': '', 'data_de_expiracao': [('12/02/2025', 'primeira data de exipracao da barra de cera'), ('12/02/2026', 'segunda data de exipracao da barra de cera')]}
+    print(generate_barcode())
+    ...
+
